e05f3ec7b4ee32922d42de2156ac2f9f33cc4b4f
Laurent Forthomme <laurent.forthomme@cern.ch>
2015-06-24 20:08:02 +0200

    Started populating the signal generator controller with simple methods

--- include/VME_SignalGenerator.h
+++ include/VME_SignalGenerator.h
@@ -2,15 +2,51 @@
 #define VME_SignalGenerator_h
 
 #include <stdint.h>
+#include <sstream>
+
+#include "CAENVMElib.h"
+#include "CAENVMEtypes.h"
+
+#include "Exception.h"
 
 namespace VME
 {
   class SignalGenerator
   {
     public:
+      enum ModuleRegister {
+        kECLLevelWrite = 0x04,
+        kNIMLevelWrite = 0x06,
+        kNIMPulseWrite = 0x08,
+        kNIMPulseRead  = 0x0A,
+        kIdentifier    = 0xFA,
+        kBoardInfo0    = 0xFC,
+        kBoardInfo1    = 0xFE
+      };
       SignalGenerator(int32_t bhandle, uint32_t baseaddr);
       inline ~SignalGenerator() {;}
+
+      unsigned short GetSerialNumber() const;
+      unsigned short GetModuleVersion() const;
+      unsigned short GetModuleType() const;
+      unsigned short GetManufacturerId() const;
+
     private:
+      /**
+       * Write a 16-bit word in the register
+       * \brief Write on register
+       * \param[in] addr register
+       * \param[in] data word
+       */
+      void WriteRegister(const ModuleRegister& reg, const uint16_t&) const;
+      /**
+       * Read a 16-bit word in the register
+       * \brief Read on register
+       * \param[in] addr register
+       * \param[out] data word
+       */  
+      void ReadRegister(const ModuleRegister& reg, uint16_t*) const;
+
       int32_t fHandle;
       uint32_t fBaseAddr;
   };
--- src/VME_SignalGenerator.cpp
+++ src/VME_SignalGenerator.cpp
@@ -6,5 +6,69 @@ namespace VME
     fHandle(bhandle), fBaseAddr(baseaddr)
   {
   }
+
+  unsigned short
+  SignalGenerator::GetSerialNumber() const
+  {
+    uint16_t word;
+    try {
+      ReadRegister(kBoardInfo1, &word);
+      return static_cast<unsigned short>(word&0xFFF);
+    } catch (Exception& e) { e.Dump(); }
+    return 0;
+  }
+
+  unsigned short
+  SignalGenerator::GetModuleVersion() const
+  {
+    uint16_t word;
+    try {
+      ReadRegister(kBoardInfo1, &word);
+      return static_cast<unsigned short>((word>>12)&0xF);
+    } catch (Exception& e) { e.Dump(); }
+    return 0;
+  }
+
+  unsigned short
+  SignalGenerator::GetModuleType() const
+  {
+    uint16_t word;
+    try {
+      ReadRegister(kBoardInfo0, &word);
+      return static_cast<unsigned short>(word&0x3FF);
+    } catch (Exception& e) { e.Dump(); }
+    return 0;
+  }
   
+  unsigned short
+  SignalGenerator::GetManufacturerId() const
+  {
+    uint16_t word;
+    try {
+      ReadRegister(kBoardInfo0, &word);
+      return static_cast<unsigned short>((word>>10)&0x3F);
+    } catch (Exception& e) { e.Dump(); }
+    return 0;
+  }
+
+  void
+  SignalGenerator::WriteRegister(const ModuleRegister& reg, const uint16_t& data) const
+  {
+    uint32_t address = fBaseAddr+reg;
+    uint16_t* fdata = new uint16_t; *fdata = data;
+    if (CAENVME_WriteCycle(fHandle, address, fdata, cvA32_U_DATA, cvD16)!=cvSuccess) {
+      std::ostringstream o; o << "Impossible to write register at 0x" << std::hex << reg;
+      throw Exception(__PRETTY_FUNCTION__, o.str(), JustWarning);
+    }
+  }
+
+  void
+  SignalGenerator::ReadRegister(const ModuleRegister& reg, uint16_t* data) const
+  {
+    uint32_t address = fBaseAddr+reg;
+    if (CAENVME_ReadCycle(fHandle, address, data, cvA32_U_DATA, cvD16)!=cvSuccess) {
+      std::ostringstream o; o << "Impossible to read register at 0x" << std::hex << reg;
+      throw Exception(__PRETTY_FUNCTION__, o.str(), JustWarning);
+    }
+  } 
 }
