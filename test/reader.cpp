e4b4cdd78e0867b9ed54497b577d3801aeeea100
Laurent Forthomme <forthomme@apinc.org>
2015-06-24 17:43:57 +0200

    ToT extraction in TDCMeasurement now takes into account the counter overflow Added ROOT for tests ; moved them to a dedicated folder

--- test/reader.cpp
+++ test/reader.cpp
@@ -2,6 +2,9 @@
 
 #include "FileReader.h"
 
+#include "TCanvas.h"
+#include "TH1.h"
+
 using namespace std;
 using namespace VME;
 
@@ -19,18 +22,27 @@ main(int argc, char* argv[])
 
   TDCMeasurement m;
   int num_events;
+  TH1D* hist_tot = new TH1D("tot", "", 100000, 0., 10000.);
   
   FileReader f(argv[1], VME::CONT_STORAGE);
   //cout << f.GetNumTDCs() << " TDCs recorded" << endl;
   num_events = 0;
   try {
     while (f.GetNextMeasurement(channel_id, &m)) {
-      cout << m.GetTrailingTime()-m.GetLeadingTime() << endl;
+      hist_tot->Fill(m.GetToT()*25./1024.);
       //m.Dump();
       num_events++;
     }
   } catch (Exception& e) { e.Dump(); }
   cerr << "number of events in channel " << channel_id << ": " << num_events << endl;
   
+  TCanvas* c = new TCanvas;
+  hist_tot->Draw();
+  hist_tot->GetXaxis()->SetTitle("Time over threshold (ns)");
+  hist_tot->GetYaxis()->SetTitle("Events");
+  c->SaveAs("dist_tot.png");
+  c->SetLogy();
+  c->SaveAs("dist_tot_logscale.png");
+
   return 0;
 }
