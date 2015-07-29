790206ee276a7dda971d86d3df0e7ff60e208b1e
Alex Toldaiev <oleksii.toldaiev@gmail.com>
2015-07-29 17:47:31 +0200

    Added proper handshawe in WebClient, corrected the page a little.

--- test/servesocket.py
+++ test/servesocket.py
@@ -5,17 +5,28 @@ import websockets
 
 @asyncio.coroutine
 def hello(websocket, path):
-    while True:
-        name = yield from websocket.recv()
-        print("< {}".format(name))
-        greeting = "Hello {}!".format(name)
-        if not websocket.open:
-        	break
-        yield from websocket.send(greeting)
-        print("> {}".format(greeting))
+    print("starting server")
+    while websocket.open:
+        data = yield from websocket.recv()
+        message = str(data).strip()
+        # print(message, type(message))
+        print("< {}".format(message))
 
-start_server = websockets.serve(hello, 'localhost', 8765)
+        if "ADD_CLIENT:1" in message:
+            reply = "SET_CLIENT_ID:9"
+        else:
+            reply = "Hello {}!".format(message)
+
+        if not websocket.open:
+            break
+        yield from websocket.send(reply)
+        print("> {}".format(reply))
+
+start_server = websockets.serve(hello, 'localhost', 1987)
+
+print("a;ldkf")
 
 asyncio.get_event_loop().run_until_complete(start_server)
+print("a;ldkf")
 asyncio.get_event_loop().run_forever()
-
+print("a;ldkf")
--- web/control-behavior.js
+++ web/control-behavior.js
@@ -8,7 +8,22 @@ var built_clients;
 var acquisition_started;
 
 
-
+function parse_handshake_message(event) {
+  var d = event.data //.slice(0,-1); // CHECK: is slice needed?
+  m = d.split(":");
+  switch (m[0]) {
+    case "SET_CLIENT_ID":
+          listener_id = parseInt(m[1]);
+          $( "#socket_id" ).val( listener_id );
+          socket_refresh();
+          setup_connection(connection);
+          interface_on();
+          image_changer = setInterval(change_image, 5000); // TODO: kick when DQM is ready
+          break;
+    default:
+          append_to_console( '<p>wierd message @ handshake<\p>' );
+  }
+}
 
 
 function parse_message(event) {
@@ -16,47 +31,47 @@ function parse_message(event) {
   m = d.split(":");
   switch (m[0]) {
     case "SET_CLIENT_ID":
-      listener_id = parseInt(m[1]);
-      $( "#socket_id" ).val( listener_id );
-      socket_refresh();
-      break;
+          listener_id = parseInt(m[1]);
+          $( "#socket_id" ).val( listener_id );
+          socket_refresh();
+          break;
     case "CLIENTS_LIST":
-      var listeners = m[1].split(';');
-      // TODO: do something
-      break;
+          var listeners = m[1].split(';');
+          // TODO: do something
+          break;
     case "THIS_CLIENT_DELETED":
-      unbind_socket();
-      break;
+          unbind_socket();
+          break;
     case "OTHER_CLIENT_DELETED":
-      if (m[1]==listener_id) {
-        unbind_socket();
-      }
-      else append_to_console("<p>Socket " + m[1]+" successfully deleted!</p>");
-      break;
+          if (m[1]==listener_id) {
+            unbind_socket();
+          }
+          else append_to_console("<p>Socket " + m[1]+" successfully deleted!</p>");
+          break;
     case "PING_ANSWER":
-      alert(m[1]);
-      break;
+          alert(m[1]);
+          break;
     case "ACQUISITION_STARTED":
-      append_to_console( "<p>Acquisition process successfully launched!</p>" );
-      alert("Acquisition process successfully launched!");
-      $( "#acquisition_button" ).html( "Stop acquisition" );
-      $( "#acquisition_button" ).click( stop_acquisition) ;
-      acquisition_started = true;
-      break;
+          append_to_console( "<p>Acquisition process successfully launched!</p>" );
+          alert("Acquisition process successfully launched!");
+          $( "#acquisition_button" ).html( "Stop acquisition" );
+          $( "#acquisition_button" ).click( stop_acquisition) ;
+          acquisition_started = true;
+          break;
     case "ACQUISITION_STOPPED":
-      append_to_console( "<p>Acquisition process terminated!</p>" );
-      alert("Acquisition process terminated!");
-      $("#acquisition_button").html( "Start acquisition" );
-      $("#acquisition_button").click( start_acquisition );
-      acquisition_started = false;
-      break;
+          append_to_console( "<p>Acquisition process terminated!</p>" );
+          alert("Acquisition process terminated!");
+          $("#acquisition_button").html( "Start acquisition" );
+          $("#acquisition_button").click( start_acquisition );
+          acquisition_started = false;
+          break;
     case "EXCEPTION":
-      append_to_console( "<p>" + d + "</p>" );
-      break;
+          append_to_console( "<p>" + d + "</p>" );
+          break;
     case "MASTER_DISCONNECT":
-      alert("ALERT:\nMaster disconnected!");
-      // restore_init_state();
-      break;
+          alert("ALERT:\nMaster disconnected!");
+          // restore_init_state();
+          break;
   }
 }
 
@@ -174,10 +189,11 @@ function unbind_socket() {
   send_message(connection, "REMOVE_CLIENT:" + listener_id);
   // connection.send("REMOVE_CLIENT:"+listener_id);
   connection.close();
-  // connection = undefined;
+  connection = undefined;
 
   // interface_off();
-  alert( "UNBOUNDED!" );
+  // alert( "UNBOUNDED!" );
+  append_to_console( '<p>UNBOUNDED!<\p>' );
   // connection = undefined;
   // connection.onmessage = function(event) { parse_message(event); }
 }
@@ -198,7 +214,7 @@ function socket_refresh() {
 
 
 function start_acquisition() {
-  if (connection===0) alert("no connection initiated");
+  // if (connection===0) alert("no connection initiated");
   send_message(connection, "START_ACQUISITION:" + listener_id );
   // connection.send("START_ACQUISITION:"+listener_id);
   // connection.onmessage = function(event) { parse_message(event); } // CHECK: why use it? we set the handler on creation of the connection
@@ -210,42 +226,7 @@ function stop_acquisition() {
 
 
 
-
-
-function bind_socket() {
-  connection_uri = $("#connection_uri").val();
-  // hostname = $("#hostname").val();
-
-  // alert( connection );
-  // alert( hostname + ":" + port );
-  // if (connection==0) { alert("no connection") };
-  alert("START BIND");
-
-  if (typeof connection !== "undefined") {
-    alert("repeated connection 2");
-    console.error("Wrong (or multiple) attempt made to bind! Aborting.");
-    // connection = 0;
-    return;
-  }
-
-  alert("CONNECTION IS UNDEFINED");
-
-  // hostname = 'localhost';
-  //hostname = '137.138.105.83';
-  try {
-    connection = new WebSocket(connection_uri);
-  }
-  catch (e) {
-    alert(e);
-  }
-
-  alert("NEW CONNECTION");
-  // alert( connection );
-  // alert( connection );
-
-  // bind_button.disabled = true;
-  // built_clients = [];
-
+function setup_connection(connection) {
   connection.onopen = function () {
     // what a hell
     append_to_console( '<p class="incoming">GOT: Connection opened<\p>' );
@@ -290,10 +271,61 @@ function bind_socket() {
       // alert( "socket onclose" );
   };
 
-  interface_on();
-  image_changer = setInterval(change_image, 5000);
+  append_to_console( '<p>CONNECTION HANDLERS ARE SET UP<\p>' );
+  // alert("CONNECTION HANDLERS ARE SET UP");
+}
 
-  alert("CONNECTION HANDLERS ARE SET UP");
+
+
+
+
+function bind_socket() {
+  connection_uri = $("#connection_uri").val();
+  // hostname = $("#hostname").val();
+
+  // alert( connection );
+  // alert( hostname + ":" + port );
+  // if (connection==0) { alert("no connection") };
+  append_to_console( '<p>Start Bind<\p>' );
+
+  if (typeof connection !== "undefined") {
+    alert("repeated connection 2");
+    console.error("Wrong (or multiple) attempt made to bind! Aborting.");
+    // connection = 0;
+    return;
+  }
+
+  append_to_console( '<p>Connection is undefined<\p>' );
+
+  // hostname = 'localhost';
+  //hostname = '137.138.105.83';
+  try {
+    connection = new WebSocket(connection_uri);
+  }
+  catch (e) {
+    alert(e);
+  }
+
+  append_to_console( '<p>New connection created:' + connection + '<\p>' );
+
+  // prepare handshake response
+  connection.onmessage = function(event) {
+    append_to_console( '<p class="incoming">GOT @ handshake: ' + event.data + "<\p>" );
+    //SET_CLIENT_ID:ID
+    parse_handshake_message(event); // if it receives SET_CLIENT_ID:ID the connection is set up for run
+  };
+  connection.onopen = function(event) {
+    append_to_console( '<p>Connection opened @ handshake<\p>' );
+    send_message( connection, "ADD_CLIENT:1" ); // Handshake call
+    append_to_console( '<p>Sent the handshake<\p>' );
+  }
+  connection.onerror = function(event) {
+    append_to_console( '<p>Connection error @ handshake<\p>' );
+    // TODO: print the error message
+  }
+  connection.onclose = function(event) {
+    append_to_console( '<p>Connection closed @ handshake<\p>' );
+  }
 }
 
 
@@ -304,7 +336,7 @@ function interface_on() {
       $( "#unbind_button" ).click( unbind_socket );
       $( "#refresh_button" ).click( socket_refresh );
       $( "#acquisition_button" ).click( start_acquisition );
-      $( "input" ).prop("disabled", true);
+      $( "input#connection_uri" ).prop("disabled", true);
       $( "#bind_button, #unbind_button, #refresh_button, #acquisition_button" ).toggleClass( "enabled" );
 }
 
@@ -314,7 +346,7 @@ function interface_off() {
       // disable interface to ppsFetch process
       $("#acquisition_button").html( "Start acquisition" ); // reset acquisition button
       $( "#unbind_button, #refresh_button, #acquisition_button" ).unbind();
-      $( "input" ).prop("disabled", false);
+      $( "input#connection_uri" ).prop("disabled", false);
       $( "#bind_button, #unbind_button, #refresh_button, #acquisition_button" ).toggleClass( "enabled" );
 }
 
