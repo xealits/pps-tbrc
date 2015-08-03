2760302e42b475f141871a245352e80b78f53d77
Laurent Forthomme <laurent.forthomme@cern.ch>
2015-08-03 11:17:02 +0200

    Added a new python object to handle socket connections (perform handshake, ...)

---
+++ scripts/SocketHandler.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+import socket
+import sys
+
+class SocketHandler:
+  class SocketError(Exception):
+    pass
+  class SendingError(Exception):
+    pass
+  class InvalidMessage(Exception):
+    pass
+  
+  def __init__(self, host, port):
+    try:
+      self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    except socket.error:
+      print "Failed to create socket!"
+      sys.exit()
+    print "Socket created"
+
+    try:
+      remote_ip = socket.gethostbyname(host)
+    except socket.gaierror:
+      print "Hostname could not be resolved! Exiting"
+      sys.exit()
+
+    # connect to remote server
+    infos = (remote_ip, port)
+    self.socket.connect(infos)
+
+  def Handshake(self, client_type):
+    try:
+      self.Send('ADD_CLIENT', client_type)
+    except SocketHandler.SendingError:
+      print "Impossible to send client type. Exiting"
+      sys.exit()
+    try:
+      msg = self.Receive(4096)
+    except ReceivingError:
+      print "Socket receiving error during handshake. Exiting"
+      sys.exit()
+    except InvalidMessage:
+      print "Invalid message received in handshake. Exiting"
+      sys.exit()
+    if msg[0]=='SET_CLIENT_ID':
+      self.client_id = msg[1]
+      print 'Client id='+ self.client_id
+
+  def Receive(self, size):
+    try:
+      recv = self.socket.recv(size)
+    except socket.error:
+      raise ReceivingError
+    if ':' not in recv:
+      raise InvalidMessage
+    (key, value) = recv.split(':')
+    return (key, value)
+
+  def Send(self, key, value):
+    try:
+      self.socket.sendall(key+':'+str(value))
+    except socket.error:
+      raise SendingError
--- test/dqmer.py
+++ test/dqmer.py
@@ -1,33 +1,9 @@
 #Socket client example in python
  
-import socket   #for sockets
 import sys  #for exit
 sys.path.append("../scripts")
+import SocketHandler
 import DQMReader
-
-def make_client( host, port ):
-
-    #create an INET, STREAMing socket
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    except socket.error:
-        print 'Failed to create socket'
-        sys.exit()
-
-    print 'Socket Created'
-
-    try:
-        remote_ip = socket.gethostbyname( host )
-    except socket.gaierror:
-        #could not resolve
-        print 'Hostname could not be resolved. Exiting'
-        sys.exit()
-
-    #Connect to remote server
-    s.connect((remote_ip , port))
-    return s
-
-
 
 def dummy_computing(filename):
     reader = DQMReader.DQMReader(filename)
@@ -37,30 +13,30 @@ def dummy_computing(filename):
 
 def run_dqm_process( messenger_socket, computation_process = dummy_computing ):
     while True: # while socket and connection are alive, but, as people say, it is hard to check
-        incomming_message = ""
+        incoming_message = ""
         result_filename = ""
 
         # RECEIVE
-        incomming_message = messenger_socket.recv(4096)
-        tokens = incomming_message.split(":")
+        incoming_message = messenger_socket.Receive(4096)
 
         # COMPUTE
-        if len(tokens) == 2 and tokens[0] == "NEW_FILENAME":
-            result_filename = computation_process( tokens[1] )
-        else:
+        if len(incoming_message) != 2:
             print "Got wrong message:\n%s" % incomming_message # OR send error to the messenger?
             # MAYBE add 1 more message -- "KILL_DQM" -- and stop DQM process with it?
             continue
+        if incoming_message[0] == "NEW_FILENAME":
+            result_filename = computation_process(incoming_message[1])
 
         # SEND
-        try :
-            messenger_socket.sendall( "NEW_DQM_PLOT:" + result_filename )
-        except socket.error:
+        try:
+            messenger_socket.Send("NEW_DQM_PLOT", result_filename)
+        except SocketHandler.SendingError:
             #Send failed
             print 'Send failed'
             sys.exit()
 
 if __name__ == '__main__':
     host = "localhost"
-    socket_to_messenger = make_client( host, 1987 )
+    socket_to_messenger = SocketHandler.SocketHandler( host, 1987 )
+    socket_to_messenger.Handshake( 4 ) # 4 = DQM
     run_dqm_process( socket_to_messenger )
