fa464493f6edbc5c1bc7360851981b230d9535c8
Laurent Forthomme <forthomme@apinc.org>
2015-08-14 01:25:56 +0200

    Reorganized loop on events to simplify steps once event type is retrieved

--- test/write_tree_sorted.cpp
+++ test/write_tree_sorted.cpp
@@ -20,27 +20,24 @@ int main(int argc, char* argv[]) {
   string output = "output_test_sorted.root";
   if (argc>5) output = argv[5];
 
-  unsigned int fNumMeasurements, fNumErrors;
-  unsigned int fRunId, fBurstId;
-  unsigned long fETTT;
-  unsigned int fTriggerNumber;
-  unsigned int fEventID;
-  unsigned int fChannelId[MAX_MEAS];
+  int fNumMeasurements, fNumErrors;
+  int fRunId, fBurstId;
+  int fETTT, fTriggerNumber;
+  int fChannelId[MAX_MEAS];
   double fLeadingEdge[MAX_MEAS], fTrailingEdge[MAX_MEAS], fToT[MAX_MEAS];
 
   TFile* f = new TFile(output.c_str(), "recreate");
   TTree* t = new TTree("tdc", "List of TDC measurements");
-  t->Branch("num_measurements", &fNumMeasurements, "num_measurements/i");
-  t->Branch("num_errors", &fNumErrors, "num_errors/i");
-  t->Branch("run_id", &fRunId, "run_id/i");
+  t->Branch("num_measurements", &fNumMeasurements, "num_measurements/I");
+  t->Branch("num_errors", &fNumErrors, "num_errors/I");
+  t->Branch("run_id", &fRunId, "run_id/I");
+  //t->Branch("burst_id", &fBurstId, "burst_id/I"); // need to be clever for this...
   t->Branch("channel_id", fChannelId, "channel_id[num_measurements]/I");
-  //t->Branch("burst_id", &fBurstId, "burst_id/i"); // need to be clever for this...                                                                                                                                 
-  t->Branch("ettt", &fETTT, "ettt/l");
   t->Branch("leading_edge", fLeadingEdge, "leading_edge[num_measurements]/D");
   t->Branch("trailing_edge", fTrailingEdge, "trailing_edge[num_measurements]/D");
   t->Branch("tot", fToT, "tot[num_measurements]/D");
-  t->Branch("event_id",&fEventID, "event_id/I");
-  t->Branch("trigger_number",&fTriggerNumber,"trigger_number/i");
+  t->Branch("ettt", &fETTT, "ettt/I");
+  t->Branch("trigger_number",&fTriggerNumber,"trigger_number/I");
   
   ostringstream search1, search2, file;
   search2 << "_board" << board_id << ".dat";
@@ -89,38 +86,39 @@ int main(int argc, char* argv[]) {
 	  fTriggerNumber = num_triggers;
 	  if(num_triggers % 1000 == 0)
 	    cout << "Triggers received: " << num_triggers << endl;
-	  fNumMeasurements = 0;
+	  fNumMeasurements = fNumErrors = 0;
+          if (num_triggers>trigger_stop and trigger_stop>0) break;
+          else if (num_triggers<trigger_start) continue;
 	  //	  cout << "GlobalHeader, trigger number " << fTriggerNumber << endl;
         }
 
-        if (num_triggers>trigger_stop and trigger_stop>0) break;
-
-        if (num_triggers>=trigger_start and e.GetType()==VME::TDCEvent::TDCMeasurement and !e.IsTrailing()) {
+        else if (e.GetType()==VME::TDCEvent::TDCMeasurement) {
           unsigned int ch_id = e.GetChannelId();
-          has_leading_per_trigger[ch_id] =  e.GetTime()*25./1024.;
-	  //	  cout << "\tTDCMeasurement Leading edge: channel " << ch_id << ": " << has_leading_per_trigger[ch_id] << endl;
+          if (!e.IsTrailing()) {
+            has_leading_per_trigger[ch_id] =  e.GetTime()*25./1024.;
+	    //	  cout << "\tTDCMeasurement Leading edge: channel " << ch_id << ": " << has_leading_per_trigger[ch_id] << endl;
+          }
+          else {
+	    double trailing_time = e.GetTime()*25./1024.;
+	    //	  cout << "\tTDCMeasurement Trailing edge: channel " << ch_id << ": " << trailing_time << endl;
+	    //	  cout << "\tTDCMeasurement Trailing edge: channel " << ch_id << ": " << trailing_time-has_leading_per_trigger[ch_id] << endl;
+            if (has_leading_per_trigger[ch_id]==0.) continue;
+
+	    fChannelId[fNumMeasurements] = ch_id;
+	    fLeadingEdge[fNumMeasurements] = has_leading_per_trigger[ch_id];
+	    fTrailingEdge[fNumMeasurements] = trailing_time;
+	    fToT[fNumMeasurements] = fTrailingEdge[fNumMeasurements]-fLeadingEdge[fNumMeasurements];
+            num_channel_measurements[ch_id]++;
+            fNumMeasurements++;
+            has_leading_per_trigger[ch_id] = 0.;
+          }
+        }
+        else if (e.GetType()==VME::TDCEvent::ETTT) {
+          fETTT = e.GetETTT();
         }
 	
-        if (num_triggers>=trigger_start and e.GetType()==VME::TDCEvent::TDCMeasurement and e.IsTrailing()) {
-          unsigned int ch_id = e.GetChannelId();
-	  double trailing_time = e.GetTime()*25./1024.;
-	  //	  cout << "\tTDCMeasurement Trailing edge: channel " << ch_id << ": " << trailing_time << endl;
-	  if(has_leading_per_trigger[ch_id] != 0)
-	    {
-	      fChannelId[fNumMeasurements] = ch_id;
-	      fLeadingEdge[fNumMeasurements] = has_leading_per_trigger[ch_id];
-	      fTrailingEdge[fNumMeasurements] = trailing_time;
-	      fToT[fNumMeasurements] = fTrailingEdge[fNumMeasurements]-fLeadingEdge[fNumMeasurements];
-              num_channel_measurements[ch_id]++;
-	      fNumMeasurements++;
-	    }
-        }
-
-	if(e.GetType()==VME::TDCEvent::ETTT) {
-	  fETTT = e.GetETTT();
-	}
-	
-        if (e.GetType()==VME::TDCEvent::GlobalTrailer) {
+        else if (e.GetType()==VME::TDCEvent::GlobalTrailer) {
+          //        cout << "GlobalTrailer, trigger number " << fTriggerNumber << endl;
           for (unsigned int i=0; i<num_channels; i++) {
             if (num_channel_measurements[i]==0) continue;
 	    //	    cout << "GlobalTrailer: channel num. measurements " << i << ": " << num_channel_measurements[i] << endl;
